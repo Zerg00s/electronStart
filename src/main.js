c0c237a31c293662236cfe2f5bf7868b7ba151fd
Denis Molodtsov <Denis Molodtsov>
2016-11-14 20:50:15 -0500

    first electron app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 const electron = require('electron'); //electron-prebuilt
+const url = require('url')
+const path = require('path')
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
 app.on('ready', function(){
-   new BrowserWindow({
+   mainWindow = new BrowserWindow({
        height: 400,
        width: 400
    });
+   
+mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'home.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+mainWindow.on('closed', function(){
+    console.log('closed');
+    mainWindow = null;
+})
+
 });
